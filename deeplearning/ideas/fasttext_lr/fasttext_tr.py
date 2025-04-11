bug fix: softmax

deeplearning/ideas/fasttext_lr/fasttext_tr.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.metrics import f1_score, classification_report
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import normalize 
-from utils import *
+import utils.utils as utils
 
 SPLIT = 0.8
 TRAIN = True
@@ -44,5 +44,5 @@ if TRAIN:
 if TEST:
     clf = fasttext.load_model('fasttext'+str(0)+'.bin')
     test_pr = [clf.predict(x)[0][0].split('__')[-1] for x in X_test]
-    out(test_pr, path='./fasttext_pre_res.csv')
+    utils.out(test_pr, path='./fasttext_pre_res.csv')
     #test_pred = np.column_stack((test_pred, test_pred_))  # 将矩阵按列合并

deeplearning/ideas/textcnn/textcnn.py
@@ -37,4 +37,4 @@ class TextCNN(nn.Module):
         # fulling connect
         out = self.fc(cat)
 
-        return self.softmax(out)
+        return out